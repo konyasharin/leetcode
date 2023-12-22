9824087ef8b89711f6ff537be3bf2fcb669f0534
Nikolay <kolechka.malyshev.04@gmail.com>
2023-12-23 02:20:11 +0700

    first commit

---
+++ addTwoNumbers.ts
@@ -0,0 +1,16 @@
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
+}
+
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  let i: number
+  for(i = 0; i < Math.min(l1, l2); i ++){
+
+  }
+};
